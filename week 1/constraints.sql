202925f7765be1914b1bc539782b0cb8dd29efa3
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-09-20 15:34:54 +0530

    Create constraints.sql

---
+++ week 1/constraints.sql
@@ -0,0 +1,14 @@
+create table employee
+(emp_id int primary key auto_increment,
+  name varchar(20) not null ,
+  salary decimal(10,2)  check(salary>0),
+  dep_id int,
+  foreign key(dep_id) references departments(dep_id)
+  );
+
+
+primary key-- not null and unique both in single words
+foreign key-- link two table using some common id and create a reference between two 
+check -- it will check the condition if the condition is false then it will not insert the data
+not null-- value should be entered
+  
