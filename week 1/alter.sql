61be381326f7b042a239ba7a235b60dc77c16c5a
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-09-20 15:25:22 +0530

    Update and rename ad to alter.sql

---
+++ week 1/alter.sql
@@ -0,0 +1,11 @@
+to add column:
+    alter table <table-name> add <column-name> <datatype> <other things like null not null>;
+to change the data type of the column:
+    alter tabel <table-name> modify <column-name> <new datatype and other things>;
+to drop a column:
+    alter table <table-name> drop <column-name>;
+to rename a column:
+    alter table<table-name> rename column old_col_name to new_col_name;
+to rename a table:
+    alter table <table-name> rename to <new-table-name>;
+
