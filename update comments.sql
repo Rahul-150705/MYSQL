07d380d2e9d93c2aab08b7f3c23f80301db2dced
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-09-20 15:16:09 +0530

    Create update comments.sql

---
+++ update comments.sql
@@ -0,0 +1,3 @@
+to update the value of the table:
+        update <table-name> set <column-name>=a where <column-name> (mark) >90; 
+                    update<table-name> set <col1>="rahul",<col2>="a" where mark>95;
