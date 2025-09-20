404f4c7fde49032746736a718f0d72f9dab4a911
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-09-20 15:29:16 +0530

    Create drop.sql

---
+++ week 1/drop.sql
@@ -0,0 +1,7 @@
+to drop table:
+    drop table <table-name>;
+to drop database:
+    drop database <database-name>;
+truncate: 
+    to drop the values in table:
+        truncate table <table-name>;
